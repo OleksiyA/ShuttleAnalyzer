c220b520e330c661a70684ad39eec6314ccfa2c4
Oleksiy Ivanov <oleksiy@effectivedeveloper.com>
2013-05-01 21:10:49 +0300

    Initial Commit

---
+++ ShuttleAnalyzer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ShuttleAnalyzer
+//
+//  Created by Oleksiy Ivanov on 5/1/13.
+//  Copyright (c) 2013 Oleksiy Ivanov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
